c0b9da491f6dd4b59d82ff3f778c779401e5025b
feijaoozin <gonardo1234@gmail.com>
2026-06-07 01:23:49 -0300

    feat: garantindo status bar dark

--- App.tsx
+++ App.tsx
@@ -1,10 +1,13 @@
+import { StatusBar } from "expo-status-bar";
 import Toast from "react-native-toast-message";
+
 import { toastConfig } from "./src/components/ToastConfig";
 import Home from "./src/pages/Home";
 
 export default function App() {
   return (
     <>
+      <StatusBar style="dark" />
       <Home />
       <Toast config={toastConfig} position="top" topOffset={115} />
     </>
